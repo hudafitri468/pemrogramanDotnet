0ab5171c55084c8d30ca4701ced5afb8f16896fa
hudafitri468 <hudafitri468@gmail.com>
2023-04-03 04:40:33 +0700

    perbaikan data

--- AbsensiProject/Program.cs
+++ AbsensiProject/Program.cs
@@ -1,7 +1,32 @@
+using System.Text.Json.Serialization;
+using WebApi.Helpers;
+using WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// add services to DI container
+{
+    var services = builder.Services;
+    var env = builder.Environment;
+ 
+    services.AddDbContext<DataContext>();
+    services.AddCors();
+    services.AddControllers().AddJsonOptions(x =>
+    {
+        // serialize enums as strings in api responses (e.g. Role)
+        x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+        // ignore omitted parameters on models to enable optional params (e.g. User update)
+        x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+    services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+    // configure DI for application services
+    services.AddScoped<IUserService, UserService>();
+}
 
 var app = builder.Build();
 
